c3bfd37888369eea600fde4823d5162323e9b050
Kevin P. Kucharczyk <kevinkucharczyk@gmail.com>
2016-08-06 20:31:54 +0200

    Modify deploy script

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -8,15 +8,21 @@ REPO=`git config remote.origin.url`
 SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
 SHA=`git rev-parse --verify HEAD`
 
+# Clone the existing gh-pages for this repo into out/
+# Create a new empty branch if gh-pages doesn't exist yet (should only happen on first deply)
+git clone $REPO out
+cd out
+git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
+cd ..
+
+# Clean out existing contents
+rm -rf out/**/* || exit 0
+
+bundle exec jekyll --destination out
+
+cd out
 git config user.name "Travis CI"
 git config user.email "$COMMIT_AUTHOR_EMAIL"
-
-cd _site
-
-git init
-
-git config user.name "Travis CI Build (kevinkucharczyk)"
-git config user.email "kevinkucharczyk+travisci@gmail.com"
 
 git add .
 git commit -m "Deploy to GitHub Pages: ${SHA}"
