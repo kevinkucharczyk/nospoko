e369aadcef60f69df8cea889472babe1e6da5bbb
Travis CI <kevinkucharczyk+travisci@gmail.com>
2016-08-06 18:35:46 +0000

    Deploy to GitHub Pages: 014fec788fa9f47fb0c76dc21837881a21b25d7d

---
+++ ci/deploy.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+set -e
+
+SOURCE_BRANCH="master"
+TARGET_BRANCH="gh-pages"
+
+REPO=`git config remote.origin.url`
+SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
+SHA=`git rev-parse --verify HEAD`
+
+# Clone the existing gh-pages for this repo into out/
+# Create a new empty branch if gh-pages doesn't exist yet (should only happen on first deply)
+git clone $REPO out
+cd out
+git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
+cd ..
+
+# Clean out existing contents
+rm -rf out/**/* || exit 0
+
+bundle exec jekyll build --destination out
+
+cd out
+git config user.name "Travis CI"
+git config user.email "$COMMIT_AUTHOR_EMAIL"
+
+git add .
+git commit -m "Deploy to GitHub Pages: ${SHA}"
+
+# Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
+ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
+ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
+ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
+ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in deploy_key.enc -out deploy_key -d
+chmod 600 deploy_key
+eval `ssh-agent -s`
+ssh-add deploy_key
+
+echo "Pushing to GitHub..."
+
+# Now that we're all set up, we can push.
+git push --force $SSH_REPO $TARGET_BRANCH
+
+echo "Done!"
